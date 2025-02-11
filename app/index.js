0c13a6371bc43ed203d6e09ce6779e2ed6035bca
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-11 13:41:50 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,57 +1,6 @@
-/*
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.listen(port, () => {
-  console.log(Server running on port ${port});
-});
-
-*/
-
-/*
-#require("dotenv").config();
-const express = require("express");
-const serverless = require("@vendia/serverless-express"); // Use this package for Lambda compatibility
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Define a simple route
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-// Export the app as a Lambda handler
-module.exports.handler = serverless(app);
-*/
-
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-const express = require("express");
-const serverless = require("@vendia/serverless-express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-module.exports.handler = serverless(app);
-
-
-
-
-
-
-
-
-
-
+exports.handler = async (event) => {
+  return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello, World!" }),
+  };
+};
